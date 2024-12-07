3db339e7157c5fe5b31e83c13b9d15bbaa314455
shreyabhat20 <15shreyabhat@gmail.com>
2024-12-07 18:12:17 +0530

    Add files via upload
    Responsive landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+// Change Navbar Background Color on Scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
